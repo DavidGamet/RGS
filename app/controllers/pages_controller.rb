d3a0e411ecec7ddd7f9bd2975222fb68fb9509cb
David Gamet <atelierdufilm@orange.fr>
2018-12-17 19:25:28 +0100

    routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,23 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def voyages
+  end
+
+  def stages
+  end
+
+  def photos
+  end
+
+  def prints
+  end
+
+  def occasions
+  end
+
+  def femmes
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+ get 'voyages', to: 'pages#voyages'
+ get 'stages', to: 'pages#stages'
+ get 'prints', to: 'pages#prints'
+ get 'occasions', to: 'pages#occasions'
+ get 'femmes', to: 'pages#femmes'
  root to: 'pages#home'
 end
