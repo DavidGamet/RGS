ecd2c1e61fddf2a4c96e25f688224e42b7392dd8
David Gamet <atelierdufilm@orange.fr>
2018-12-18 17:11:19 +0100

    crud voyage

--- app/controllers/voyages_controller.rb
+++ app/controllers/voyages_controller.rb
@@ -1,4 +1,6 @@
 class VoyagesController < ApplicationController
+before_action :set_voyage, only: [:show, :edit, :update, :destroy]
+
   def index
     @voyages = Voyage.all
   end
@@ -7,17 +9,38 @@ class VoyagesController < ApplicationController
   end
 
   def new
+    @voyage = Voyage.new
   end
 
   def create
+    Voyage.create(voyage_params)
+      redirect_to voyages_path
   end
 
   def edit
   end
 
   def update
+    @voyage.update(voyage_params)
+    redirect_to voyages_path
   end
 
-  def delete
+
+  def destroy
+    @voyage.destroy
+    redirect_to voyages_path
   end
+
+
+private
+ def voyage_params
+   params.require(:voyage).permit(:name, :adresse, :date, :prix, :description)
+ end
+
+ def set_voyage
+  @voyage = Voyage.find(params[:id])
+ end
+
+
+
 end
