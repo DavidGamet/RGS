70b5f46f240d4af4643bf499cf21715f06318a5b
David Gamet <atelierdufilm@orange.fr>
2019-01-15 17:26:38 +0100

    essai login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
  resources :voyages
  resources :photos
